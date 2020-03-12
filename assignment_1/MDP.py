76ff192cebc6ea4f7e50b374733e39e3b7ee8434
Jason <c170127@e.ntu.edu.sg>
2020-03-12 21:49:29 +0800

    add MDP base class

---
+++ assignment_1/MDP.py
@@ -0,0 +1,10 @@
+class MarkovDecisionProcess:
+    def __init__(self):
+        self.states = []
+        self.actions = set()
+
+    def transition_model(self, next_state, current_state, action):
+        pass
+
+    def reward(self, state):
+        pass
