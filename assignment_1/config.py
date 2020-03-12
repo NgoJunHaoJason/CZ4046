fef62c6ebc4887ddb63e89e88a95fb083a0f7aa4
Jason <c170127@e.ntu.edu.sg>
2020-03-12 21:49:51 +0800

    shift configs into a config file

---
+++ assignment_1/config.py
@@ -0,0 +1,10 @@
+# mapping of squares to rewards
+REWARDS = {
+    ' ': -0.04,  # white square
+    'g': 1.0,  # green square
+    'b': -1.0,  # brown square
+}
+
+STARTING_POINT = (3, 2)  # 4th row, 3rd column
+
+DISCOUNT_FACTOR = 0.99
--- assignment_1/maze.py
+++ assignment_1/maze.py
@@ -1,18 +1,9 @@
-# mapping of squares to rewards
-REWARDS = {
-    'w': -0.04,  # white square
-    'g': 1.0,  # green square
-    'b': -1.0,  # brown square
-}
-
 # let maze be a 2D array
 MAZE = [
-    ['g', 'x', 'g', 'w', 'w', 'g'],
-    ['w', 'b', 'w', 'g', 'x', 'b'],
-    ['w', 'w', 'b', 'w', 'g', 'w'],
-    ['w', 'w', 'w', 'b', 'w', 'g'],
-    ['w', 'x', 'x', 'x', 'b', 'w'],
-    ['w', 'w', 'w', 'w', 'w', 'w'],
+    ['g', 'w', 'g', ' ', ' ', 'g'],
+    [' ', 'b', ' ', 'g', 'w', 'b'],
+    [' ', ' ', 'b', ' ', 'g', ' '],
+    [' ', ' ', ' ', 'b', ' ', 'g'],
+    [' ', 'w', 'w', 'w', 'b', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' '],
 ]
-
-STARTING_POINT = (3, 2)  # 4th row, 3rd column
