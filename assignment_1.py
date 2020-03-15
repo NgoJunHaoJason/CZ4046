39fbd442c233ab53c774b4c8d7a69cef47bdece8
Jason <c170127@e.ntu.edu.sg>
2020-03-16 00:33:00 +0800

    print optimal policy grid

--- assignment_1.py
+++ assignment_1.py
@@ -1,8 +1,9 @@
+import copy
 from pprint import pprint
 
 from assignment_1.algorithms import *
 from assignment_1.config import *
-from assignment_1.maze import Maze
+from assignment_1.maze import *
 
 
 def solve_MDP():
@@ -18,6 +19,7 @@ def solve_MDP():
     # result = value_iteration(maze)
 
     # for approximation of reference utilities given in instructions
+    # values obtained through trial and error
 
     maze = Maze(
         grid=GRID,
@@ -28,20 +30,36 @@ def solve_MDP():
 
     result = value_iteration(maze, 1.4)
 
+    print(
+        'result for value iteration using discount factor of',
+        0.95, 'and maximum error threshold of', 1.4
+    )
     print('number of iterations required:', result['num_iterations'])
 
     utilities, optimal_policy = result['utilities'], result['optimal_policy']
 
-    print('utility and optimal action for each state (row, column)')
+    optimal_policy_grid = copy.deepcopy(maze.grid)
+    action_text_map = {
+        MazeAction.MOVE_UP: '^',
+        MazeAction.MOVE_DOWN: 'v',
+        MazeAction.MOVE_LEFT: '<',
+        MazeAction.MOVE_RIGHT: '>',
+    }
+
+    print('utility for each state (row, column)')
     for state_position in utilities:
         if state_position[1] == 0:
             print()
 
         print(
             'at', state_position, 
-            '-utility:', '{:.3f}'.format(utilities[state_position]),
-            '-optimal action:', optimal_policy[state_position],
+            '- utility:', '{:.3f}'.format(utilities[state_position]),
         )
+
+        optimal_policy_grid[state_position[0]][state_position[1]] = \
+            action_text_map[optimal_policy[state_position]]
+
+    pprint(optimal_policy_grid)
 
 
 if __name__ == '__main__':
