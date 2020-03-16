ecd3c75a65fd16992698204a2fc6c0eea98b0712
Jason <c170127@e.ntu.edu.sg>
2020-03-16 14:04:19 +0800

    update plot size and save figure

--- assignment_1/plot.py
+++ assignment_1/plot.py
@@ -8,12 +8,17 @@ def plot_utility_vs_iteration(iteration_utilities):
         (x, y): [utility for each iteration (float)]
     }
     """
+    plt.figure(figsize=(16, 8))
+
     for state_position in iteration_utilities:
         plt.plot(iteration_utilities[state_position])
 
     plt.legend(iteration_utilities, loc='center left', bbox_to_anchor=(1, 0.5))
+
     plt.title('utility of each state at each iteration')
     plt.xlabel('iteration')
     plt.ylabel('utility estimate')
+
+    plt.savefig('assignment_1/results/utility_iteration_plot.png')
 
     plt.show()
